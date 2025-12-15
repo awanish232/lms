544f0cbee5a4d63a4ac7227527b9ee85753db35a
Awanish Thakur <awanishthakur64@gmail.com>
2025-12-15 11:45:12 +0530

    getting started

--- client/src/components/student/CallToAction.jsx
+++ client/src/components/student/CallToAction.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { assets } from "../../assets/assets";
 import { motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
+
+
 
 const CallToAction = () => {
+  const navigate = useNavigate();
   return (
     <div className="relative pt-20 pb-28 px-8 md:px-0 text-center overflow-hidden">
 
@@ -40,12 +44,12 @@ const CallToAction = () => {
           transition={{ duration: 0.6 }}
           className="flex items-center gap-5 mt-4"
         >
-          <button className="px-10 py-3 rounded-full text-white bg-gradient-to-r from-indigo-600 to-purple-600 shadow-lg shadow-indigo-300/40 hover:shadow-xl hover:-translate-y-1 transition">
+          <button onClick={() => navigate("/course-list")} className="px-10 py-3 rounded-full text-white bg-gradient-to-r from-indigo-600 to-purple-600 shadow-lg shadow-indigo-300/40 hover:shadow-xl hover:-translate-y-1 transition">
             Get Started
           </button>
 
           <button className="flex items-center gap-2 text-indigo-700 font-medium hover:underline hover:underline-offset-4">
-            Learn More
+            
             <img src={assets.arrow_icon} alt="arrow_icon" className="w-4" />
           </button>
         </motion.div>
